e221254d6a551c4f0e09415ac0fdfc629d673734
yashaws007 <130629817+yashaws007@users.noreply.github.com>
2023-08-17 17:39:28 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,16 @@
-var mongoose = require ('mongoose');
-
+var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//change the database with yours
-mongoose.connect("mongodb+srv://yaswanthmarolix:9XHYGlrf4G3WfWSi@cluster0.aqggq89.mongodb.net/?retryWrites=true&w=majority");
+// Change the database connection URL with yours
+mailto:mongoose.connect('mongodb+srv://yaswanthmarolix:<9XHYGlrf4G3WfWSi>@cluster0.aqggq89.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB successfully!');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
 
-module.exports = {mongoose};
+module.exports = { mongoose };
+
